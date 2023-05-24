c7eee0c1c77e13ffc29a82ee05867edb2c9aede2
Phong Dang <thanhphong.dang@mafc.com.vn>
2023-05-24 14:07:35 +0700

    start with https://react.dev/learn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import carImage from './img/c92b4efe01ba41acb1170da3c85e7eca_1200.jpg'
+// import shoppingList from './list/Products';
+import ShoppingList from "./list/Products";
+import { useState } from "react";
 
 function App() {
+    // let content;
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to my first app</h1>
+
+        <MyButton/><MyButton/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +25,30 @@ function App() {
         >
           Learn React
         </a>
+        <button className="App-button" onClick={handleClick}>This is a button</button>
+
+        <img src= {carImage} className= "Car" alt={carImage}/>
+
+      <img src={logo} className= "App-logo2" alt={logo}/>
+      <ShoppingList/>
       </header>
     </div>
+
+  );
+}
+function handleClick() {
+    alert('You clicked me! How dare you!');
+}
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+      setCount(count +1);
+  }
+  return (
+      <button className= "App-button" onClick={handleClick}>
+        Clicked {count} Add one
+      </button>
   );
 }
 
